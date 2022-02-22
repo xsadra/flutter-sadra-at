8ff9b8db0deb17b6aa14571bda475f916f048cf2
Sadra <31988724+xsadra@users.noreply.github.com>
2022-02-22 16:12:27 +0100

    Add 'Main layout delegate'

---
+++ sadra_at/lib/layout/main_layout_delegate.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../main/bloc/main_bloc.dart';
+
+/// {@template main_layout_delegate}
+/// A delegate for computing the layout of the main UI.
+/// {@endtemplate}
+abstract class MainLayoutDelegate extends Equatable {
+  const MainLayoutDelegate();
+
+  Widget startSectionBuilder(MainState state);
+
+  Widget middleSectionBuilder(MainState state);
+
+  Widget endSectionBuilder(MainState state);
+
+  Widget backgroundBuilder(MainState state);
+
+  Widget messageFormBuilder(MainState state);
+
+  Widget whitespaceBuilder();
+}
